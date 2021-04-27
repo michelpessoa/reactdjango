41b1408498cd22f3d130e99ba9ff39988b867188
michelpessoa <michelpessoa@gmail.com>
2021-04-26 22:14:32 -0300

    Profile List My Orders

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -19,7 +19,9 @@ import { USER_LOGIN_REQUEST,
          USER_UPDATE_PROFILE_FAIL, 
         //  USER_UPDATE_PROFILE_RESET
 
-        } from '../constants/userConstants'
+} from '../constants/userConstants'
+
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 
 
 export const login = (email, password) => async (dispatch) => {
@@ -60,6 +62,7 @@ export const login = (email, password) => async (dispatch) => {
 export const logout =  () => (dispatch) =>  {
     localStorage.removeItem('userInfo')
     dispatch({ type: USER_DETAILS_RESET})
+    dispatch({ type: ORDER_LIST_MY_RESET})
     dispatch({ type: USER_LOGOUT})
     
 }
